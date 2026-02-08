72b9e701fbad1208555f3aae2ea25dc90f29bfff
mateusz-nowacki <mattynovy@outlook.com>
2026-02-08 22:28:19 +0100

    hbjik

--- my-tldraw-yjs/server.js
+++ my-tldraw-yjs/server.js
@@ -1,13 +1,24 @@
-import { WebSocketServer } from 'ws'
-import * as Y from 'yjs'
+import { createRequire } from 'module';
 import { setupWSConnection } from 'y-websocket/bin/utils'
+import * as Y from 'yjs'
+
+const require = createRequire(import.meta.url);
+const wsModule = require('ws');
+
+// Debug log removed
+
+
+const WebSocketServer = wsModule.WebSocketServer || wsModule.Server;
+
+if (!WebSocketServer) {
+    console.error('WebSocketServer not found in ws module!');
+    process.exit(1);
+}
 
 const wss = new WebSocketServer({ port: 1234 })
 
 wss.on('connection', (conn, req) => {
-    // Log the connection URL to confirm path routing
     console.log(`Connection received: ${req.url}`)
-
     setupWSConnection(conn, req, {
         gc: true,
     })
